9ba1b33b4f053551a81042c9b31f9f8d97281daf
Bruno <bcostinha01@gmail.com>
2022-05-17 22:23:30 +0100

    Create basic component

---
+++ my-project/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import MyName from './components/MyName.vue'
+</script>
+
+<template>
+  
+  <main>
+    <MyName />
+  </main>
+</template>
+
+
+
+
---
+++ my-project/src/components/MyName.vue
@@ -0,0 +1,6 @@
+<template>
+  <h1>Hello. My name is Bruno</h1>
+  <h2>I am from Portugal</h2>
+  
+  <button @click="">Click me</button>
+</template>
